5da08ff391e19e075862658dfd6b1a2eeb8e9452
Prelang Builder <builder@prelang.com>
2016-10-30 12:53:27 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Question' to allow for create, update

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Question do
+  permit_params :question, :subject, :topic, :grade, :type_of_question, :image, :school, :owner
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
